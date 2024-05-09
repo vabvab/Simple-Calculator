65925b75fed5fd853a775b58d5558013c188fec2
Nicholas Ekanem <penicholas87@gmail.com>
2024-05-09 21:48:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function appendToDisplay(value) {
+  document.getElementById('display').value += value;
+}
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+function calculate() {
+  var displayValue = document.getElementById('display').value;
+  try {
+    var result = eval(displayValue);
+    document.getElementById('display').value = result;
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
